393b921caaed741eccedfc667eed11c0581b4164
Vignesh M <vigneshm@agiratech.com>
2019-05-13 01:48:40 +0530

    Finished UI design and functionality
    Rolled back to a previous version of audio player to overcome the issue with the plugin

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,43 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Expanded getXylo(Color color, int soundNum) {
+    return Expanded(
+      child: RaisedButton(
+        onPressed: () {
+          playSound(soundNum);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              getXylo(Colors.red, 1),
+              getXylo(Colors.orange, 2),
+              getXylo(Colors.yellow, 3),
+              getXylo(Colors.green, 4),
+              getXylo(Colors.teal, 5),
+              getXylo(Colors.blue, 6),
+              getXylo(Colors.purple, 7)
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
   }
 }
